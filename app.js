6c005c5cb0c56af25c3fb5f28a5c8779f2c0408d
ZARKUAC <tsotne.zarkua.1@btu.edu.ge>
2024-10-08 15:38:31 +0400

    mongodb +changes index

--- app.js
+++ app.js
@@ -1,29 +1,46 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const Blog = require('./models/blog');
 
 // express app
 const app = express();
+
+// connect to mongoDB
+const dbURI = 'mongodb+srv://test_user:test123@testdatabase.970me.mongodb.net/blogs-db?retryWrites=true&w=majority&appName=testdatabase';
+mongoose.connect(dbURI)
+    .then((result) => app.listen(3000))
+    .catch((err) => console.log(err));
+
+
 app.set('view engine', 'ejs');
 
-// listen for requests
-app.listen(3000);
 
 //middleware & static files
 app.use(express.static('public'));
 app.use(morgan('dev'));
 
+
+//routes
 app.get('/', (req, res) => {
-    const blogs = [
-        {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-        {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-        {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-      ];
-    res.render('index', { title: 'Home', blogs });
+    res.redirect('/blogs');
 });
 
 app.get('/about', (req, res) => {
     res.render('about', { title: 'About' });
 });
+
+//blog routes
+app.get('/blogs', (req, res) =>{
+    Blog.find().sort({ createdAt: -1 })
+     .then((result) => {
+        res.render('index', { title: 'All Blogs', blogs: result });
+     })
+     .catch((err) => {
+        console.log(err);
+     });
+});
+
 app.get('/blogs/create', (req,res) => {
     res.render('create', { title: 'Create' });
 })
