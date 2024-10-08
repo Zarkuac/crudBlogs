88b31ba8aa15e2126565d1e639592bb7ceac309a
ZARKUAC <tsotne.zarkua.1@btu.edu.ge>
2024-10-08 13:21:22 +0400

    separate styles.css +middleware

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const morgan = require('morgan');
 
 // express app
 const app = express();
@@ -6,6 +7,10 @@ app.set('view engine', 'ejs');
 
 // listen for requests
 app.listen(3000);
+
+//middleware & static files
+app.use(express.static('public'));
+app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
     const blogs = [
@@ -22,6 +27,7 @@ app.get('/about', (req, res) => {
 app.get('/blogs/create', (req,res) => {
     res.render('create', { title: 'Create' });
 })
+
 // 404 page
 app.use((req,res) =>{
     res.status(404).render('404', { title: '404 PAGE' });
